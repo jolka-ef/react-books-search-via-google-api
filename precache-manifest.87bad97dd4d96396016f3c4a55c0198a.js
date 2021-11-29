0dae4647c4b6931d94de5629f4c0b15ecf330c1b
Jolka <firjola@gmail.com>
2021-11-29 19:35:32 +0100

    Updates

---
+++ precache-manifest.87bad97dd4d96396016f3c4a55c0198a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64370a744845c1512be78ed4d0b2cbee",
+    "url": "/react-books-search-via-google-api/index.html"
+  },
+  {
+    "revision": "b00e0d3544933d77a7b7",
+    "url": "/react-books-search-via-google-api/static/css/main.2ca9d70e.chunk.css"
+  },
+  {
+    "revision": "1b1d610d10c4bbfe29e7",
+    "url": "/react-books-search-via-google-api/static/js/2.948f9318.chunk.js"
+  },
+  {
+    "revision": "b00e0d3544933d77a7b7",
+    "url": "/react-books-search-via-google-api/static/js/main.f63d3d42.chunk.js"
+  },
+  {
+    "revision": "277c37bc253ee30cc116",
+    "url": "/react-books-search-via-google-api/static/js/runtime~main.fa9dfe91.js"
+  },
+  {
+    "revision": "ba8a1f27ea65c2b4bb42e5a2cbc5ade0",
+    "url": "/react-books-search-via-google-api/static/media/placeholder.ba8a1f27.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/books-search-via-google-api/precache-manifest.d373fe288496316b6edf7ed22b110742.js"
+  "/react-books-search-via-google-api/precache-manifest.87bad97dd4d96396016f3c4a55c0198a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/books-search-via-google-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-books-search-via-google-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
