454642e6f569b47c0cb6c3422207de70e80f6c81
Jolka <firjola@gmail.com>
2023-09-26 20:06:14 +0200

    gh pages

---
+++ precache-manifest.e6962a892634668954a827a92bc44aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b92890c3ec5bed37334e3fc26422e54",
+    "url": "/react-books-search-via-google-api/index.html"
+  },
+  {
+    "revision": "366ea751463ac7ba0748",
+    "url": "/react-books-search-via-google-api/static/css/main.2ca9d70e.chunk.css"
+  },
+  {
+    "revision": "1b1d610d10c4bbfe29e7",
+    "url": "/react-books-search-via-google-api/static/js/2.948f9318.chunk.js"
+  },
+  {
+    "revision": "366ea751463ac7ba0748",
+    "url": "/react-books-search-via-google-api/static/js/main.6ad1ce16.chunk.js"
+  },
+  {
+    "revision": "277c37bc253ee30cc116",
+    "url": "/react-books-search-via-google-api/static/js/runtime~main.fa9dfe91.js"
+  },
+  {
+    "revision": "ba8a1f27ea65c2b4bb42e5a2cbc5ade0",
+    "url": "/react-books-search-via-google-api/static/media/placeholder.ba8a1f27.svg"
+  }
+]);
